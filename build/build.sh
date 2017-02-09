39d8fb766c525d4ed4913affdefbd6a27dd6b113
corsonr <remicorson@gmail.com>
2017-02-09 15:31:18 +0100

    Update cover & build process
    - Update cover
    - Update build process, add TOC

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pandoc -o ../ebooks/Livre-WooCommerce.epub ../**/*.md --epub-cover-image=cover.png --epub-metadata=metadata.xml
+pandoc -o ../ebooks/Livre-WooCommerce.epub ../**/*.md --epub-cover-image=cover.png --epub-metadata=metadata.xml --toc --toc-depth=2
 kindlegen ../ebooks/Livre-WooCommerce.epub
